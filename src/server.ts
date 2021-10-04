beffe443f3f3bfbf6a81273f4e137662ca9a0be9
rossignolli <vitorrossignolli@gmail.com>
2021-10-04 18:50:06 -0300

    SLL reject connection false

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/favicon.ico', express.static('images/favicon.ico'));
+
 require('dotenv').config();
 
 const devConfig = {
